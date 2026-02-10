efd0b1bb379f77a5c21d429a8b9eee5ab95a9e56
dahunj <dahunj@nate.com>
2026-02-10 18:56:36 +0900

    R53B 버그 픽스

--- CME8000/AviHandler.cpp
+++ CME8000/AviHandler.cpp
@@ -210,7 +210,7 @@ void CAviHandler::Get_LotStart(CString sLotID, CString sPortNo, CString sTrayCnt
 
 	if (sModel != gData.sRecipe) 
 	{
-		if (sModel != "R63B" && sModel != "R64B") { AfxMessageBox("Invalid Model Name!!!"); return; }
+		if (sModel != "R53B" && sModel != "R54B") { AfxMessageBox("Invalid Model Name!!!"); return; }
 
 		CIniFileCS INI(gsCurrentDir + "\\System\\EquipData.ini");
 		if (!INI.Check_File()) { AfxMessageBox("EquipData.ini File Not Found!!!"); return; }
