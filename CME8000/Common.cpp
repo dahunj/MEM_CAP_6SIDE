1fbd2542d04f1ea7cf6faaec03edef4e08175ed3
ORMPC-20247\Administrator <dahunj@gmail.com>
2026-03-26 16:38:18 +0900

    부자재 투입부 관련 시퀀스 변경

--- CME8000/Common.cpp
+++ CME8000/Common.cpp
@@ -394,32 +394,32 @@ BOOL CCommon::Check_TrayFull()
 
 	if (pDX01->iLoadPort3HighCheck)		
 	{ 
-		Show_MsgBox(1,"Load Port3 Full Sensor Checked.");
-		//Show_Alarm("Load Port3 Full Sensor Checked.");
+		//Show_MsgBox(1,"Load Port3 Full Sensor Checked.");
+		Show_Alarm("Load Port3 Full Sensor Checked.");
 		return FALSE; 
 	}
 	if (pDX02->iCapPort1HighCheck && !gData.bCapPort1Wait)		
 	{ 
-		Show_MsgBox(1,"Cap Port1 Full Sensor Checked.");
-		//Show_Alarm("Cap Port1 Full Sensor Checked.", STATE_CAPTRAY);
+		//Show_MsgBox(1,"Cap Port1 Full Sensor Checked.");
+		Show_Alarm("Cap Port1 Full Sensor Checked.", STATE_CAPTRAY);
 		return FALSE;
 	}
-	if (pDX02->iCapPort2HighCheck)		
+	if (pDX02->iCapPort2HighCheck && !gData.bCapPort2Wait)		
 	{ 
-		Show_MsgBox(1,"Cap Port2 Full Sensor Checked.");
-		//Show_Alarm("Cap Port1 Full Sensor Checked.", STATE_CAPTRAY);
+		//Show_MsgBox(1,"Cap Port2 Full Sensor Checked.");
+		Show_Alarm("Cap Port1 Full Sensor Checked.", STATE_CAPTRAY);
 		return FALSE;
 	}
 	if (pDX03->iUnloadPort1HighCheck && !gData.bUnloadPort1Wait)	
 	{ 
-		Show_MsgBox(1,"Unload Port1 Full Sensor Checked.");
-		//Show_Alarm("Unload Port2 Full Sensor Checked.", STATE_SHIPTRAY); 
+		//Show_MsgBox(1,"Unload Port1 Full Sensor Checked.");
+		Show_Alarm("Unload Port2 Full Sensor Checked.", STATE_SHIPTRAY); 
 		return FALSE;
 	}
 	if (pDX03->iUnloadPort2HighCheck)	
 	{ 
-		Show_MsgBox(1,"Unload Port2 Full Sensor Checked.");
-		//Show_Alarm("Unload Port2 Full Sensor Checked.", STATE_SHIPTRAY); 
+		//Show_MsgBox(1,"Unload Port2 Full Sensor Checked.");
+		Show_Alarm("Unload Port2 Full Sensor Checked.", STATE_SHIPTRAY); 
 		return FALSE;
 	}
 
