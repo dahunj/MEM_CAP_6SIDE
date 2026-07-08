2091ac64916b57e8c0328554731c1bbe20932d95
dahunj <dahunj@nate.com>
2026-07-08 12:49:32 +0900

    정리

--- CME8000/stdafx.h
+++ CME8000/stdafx.h
@@ -76,11 +76,11 @@
 	#pragma comment(lib, "CSGridR.lib")
 #endif
 
-#define MAIN_VERSION	_T("V 2.2.0.0_MEM26") //MEM26 
+#define MAIN_VERSION	_T("V 2.2.0.0 MEM26") //MEM26 
 //
-//#define AJIN_BOARD_USE
-//#define LOT_BARCODE_USE
-//#define LOAD_CELL_USE
+#define AJIN_BOARD_USE
+#define LOT_BARCODE_USE
+#define LOAD_CELL_USE
 
 
 // #define BARCODE_CHECK   	// Cap/Ship Barcode Check
