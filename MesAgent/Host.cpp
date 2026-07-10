7f1dfaf371649b05e9b025120b526165a7c2f92b
dahunj <dahunj@nate.com>
2026-07-10 09:16:05 +0900

    현장 테스트 반영

--- MesAgent/Host.cpp
+++ MesAgent/Host.cpp
@@ -646,6 +646,7 @@ void CHost::Set_S6F11_LotEnd(CString sLotId, CString sRecipe, int nCount, int nO
 	strTotal.Format("%d", nCount);
 	strReal.Format("%d", nOk + nNg);
 	strGood.Format("%d", nOk);
+	strGood.Format("%d", 0);
 	strVNg.Format("%d", nNg );	// AVI NG
 	strBNg.Format("%d", nBNg);			// Blade NG
 	
@@ -680,7 +681,7 @@ void CHost::Set_S6F11_LotEnd(CString sLotId, CString sRecipe, int nCount, int nO
 	strSend += "      <DV NAME=\"REASONCODEQTY\" VALUE=\"2\" />" + CRLF;
 	strSend += "      <DV NAME=\"REASONCODE#1\" VALUE=\"" + strRsnCode + "\" />" + CRLF;
 	strSend += "      <DV NAME=\"REASONCODECOUNT#1\" VALUE=\"" + strVNg + "\" />" + CRLF;
-	strSend += "      <DV NAME=\"REASONCODE#2\" VALUE=\"BLNG\" />" + CRLF;
+	strSend += "      <DV NAME=\"REASONCODE#2\" VALUE=\"CAPNG\" />" + CRLF;
 	strSend += "      <DV NAME=\"REASONCODECOUNT#2\" VALUE=\"" + strBNg + "\" />" + CRLF;
 	strSend += "    </DVLIST>" + CRLF;
 	strSend += "  </ITEM>" + CRLF;
