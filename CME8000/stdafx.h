0708b53a705d3474f3f5212a3d4f55f55b0524fe
ORMPC-20247\Administrator <dahunj@gmail.com>
2026-07-13 11:42:44 +0900

    버전 변경

--- CME8000/stdafx.h
+++ CME8000/stdafx.h
@@ -71,7 +71,7 @@
 	#pragma comment(lib, "CSGridR.lib")
 #endif
 
-#define MAIN_VERSION	_T("V 2.1.0.5")
+#define MAIN_VERSION	_T("V 2.1.0.5 MEM26")
 //
 #define AJIN_BOARD_USE
 #define LOT_BARCODE_USE
